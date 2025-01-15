22af8d56f4f55a3784b974719ad3522fa19ba3e2
amsali-dotcom <aliabba0002@gmail.com>
2025-01-15 20:09:14 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include "example.h"
+#include<stdio.h>
+int main(){
+	int a,b=2,c=3;
+	a=power(b,c);
+	printf("%d", a);
+}
---
+++ main2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "example.c"
+int main( void ) {
+int result = power( 2, 3 );
+printf( "2³ == %d\n", result );
+result = fact( 6 );
+printf( "6! == %d\n", result );
+return EXIT_SUCCESS;
+}
