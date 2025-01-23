e90a6770eb84d3ebdff4593549290e7df339468b
amsali-dotcom <aliabba0002@gmail.com>
2025-01-23 18:50:45 +0100

    Update example.h

--- example.h
+++ example.h
@@ -1,7 +1,19 @@
 #ifndef __MY_MATH_LIB_H
 #define __MY_MATH_LIB_H
-// Définition d'une fonction de calcul de factorielle.
+// DÃ©finition d'une fonction de calcul de factorielle.
 unsigned int fact( unsigned int value );
-// Définition d'une fonction d'élévation à une puissance données.
+// DÃ©finition d'une fonction d'Ã©lÃ©vation Ã  une puissance donnÃ©es.
 int power( int value, unsigned int pow );
+//definition de la fonction bonjour (EXERCICE 1)
+void salbonjour();
+//definition de la fonction inverse des nombres (EXERCICE 2)
+int inv(int x);
+//definition de la fonction produit de deux reels (EXERCICE 3)
+float prod( float a, float b);
+//defition de la fonction parite d'un entier (EXERCICE 4)
+void parite(int n);
+// definition de la fonction de comparaison de deux nombres entiers (EXERCICE 5)
+void comparaison(int a, int b);
+//definition de la fonction de classement de trois entiers par ordre croissant (EXERCICE 6)
+void croissant(int x, int b, int z);
 #endif
