6404aa4a2c63072fca6d4e3f7e30d1e0ccbfecef
amsali-dotcom <aliabba0002@gmail.com>
2025-01-23 19:06:16 +0100

    Update main.c

--- main2.c
+++ main2.c
@@ -3,8 +3,43 @@
 #include "example.c"
 int main( void ) {
 int result = power( 2, 3 );
-printf( "2³ == %d\n", result );
+printf( "2Â³ == %d\n", result );
 result = fact( 6 );
 printf( "6! == %d\n", result );
+  
+printf("\tEXERCICE 1\n");
+salutationbonjour();
+
+printf("\tEXERCICE 2\n");
+int nombre;
+printf("entrer un entier a trois chiffres:");
+scanf("%d", &nombre);
+int inver=inv(nombre);
+printf("l'inverse de %d est %d\n",nombre, inver);
+
+printf("\tEXERCICE 3\n");
+printf("entrer deux reels: \n");
+float a,b;
+scanf("%f%f", &a, &b);
+float p=produit(a,b);
+printf(" Le produit est %.2f\n", p);
+
+printf("\tEXERCICE 4\n");
+int n;
+printf("entrer un entier pour connaitre sa parite: ");
+scanf("%d", &n);
+parite(n);
+
+printf("\tEXERCICE 5\n");
+int c,d;
+printf("entrer deux entiers : ");
+scanf("%d%d", &c, &d);
+comparaison(c,d);
+
+printf("\tEXERCICE 6\n");
+int x,y,z;
+printf("entrer trois entiers :");
+scanf("%d%d%d", &x,&y,&z);
+croissant(x,y,z);
 return EXIT_SUCCESS;
 }
