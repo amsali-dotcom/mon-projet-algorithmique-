7ed9824da365a6d2fc328a4301c6270107f394ac
amsali-dotcom <aliabba0002@gmail.com>
2025-01-23 18:57:28 +0100

    Update example.c

--- example.c
+++ example.c
@@ -1,5 +1,5 @@
 #include "example.h"
-// Définition d'une fonction de calcul de factorielle.
+// DÃ©finition d'une fonction de calcul de factorielle.
 unsigned int fact( unsigned int value ) {
 unsigned int result = 1;
 while ( value > 1 ) {
@@ -8,7 +8,7 @@ value --;
 }
 return result;
 }
-// Définition d'une fonction d'élévation à une puissance données.
+// DÃ©finition d'une fonction d'Ã©lÃ©vation Ã  une puissance donnÃ©es.
 int power( int value, unsigned int pow ) {
 if ( pow == 0 ) return 1;
 if ( pow == 1 ) return value;int accumulator = 1;
@@ -17,4 +17,56 @@ accumulator *= value;
 pow--;
 }
 return accumulator;
+}
+//definition de la fonction bonjour (EXERCICE 1)
+void salbonjour(){
+	printf("bonjour\n");
+}
+//definition de la fonction inverse des nombres (EXERCICE 2)
+int inv(int x){
+    int inverse, centaine, dizaine, unite ;
+    if(x >= 100 && x<= 999){
+        centaine = x/100;
+        dizaine = (x/10)%10;
+        unite = x%10;
+        inverse = unite*100 + dizaine*10 + centaine;
+    }
+	return inverse;
+}
+//definition de la fonction produit (EXERCICE 3)
+float produit( float a, float b){
+	float p;
+	p=a*b;
+	return p;
+}
+//defition de la fonction parite d'un entier (EXERCICE 4)
+void parite(int n){
+	if(n%2==0)
+	printf("le nombre est paire\n");
+	else
+	printf("le nombre est impaire\n");
+}
+// definition de la fonction de comparaison de deux nombres (EXERCICE 5)
+void comparaison(int a, int b){
+	if(a<b)
+	printf("%d est inferieur a %d (%d<%d)\n",a,b,a,b);
+	else if(a=b)
+	printf("%d est egale a %d (%d=%d)\n",a,b,a,b);
+	else
+	printf("%d est superieur a %d (%d>%d)\n",a,b,a,b);
+}
+//definition de la fonction de classement de trois entiers par ordre croissant(EXERCICE 6)
+void croissant(int x, int y, int z){
+	if(x<y && y<z)
+	printf("%d<%d<%d", x,y,z);
+	else if(y<x && x<z)
+	printf("%d<%d<%d", y,x,z);
+	else if(z<x && x<y)
+	printf("%d<%d<%d", z,x,y);
+	else if(z<y && y<x)
+	printf("%d<%d<%d", z,y,x);
+	else if(x<y && z<y)
+	printf("%d<%d<%d", x,z,y);
+	else
+	printf("%d<%d<%d", y,z,x);
 }
